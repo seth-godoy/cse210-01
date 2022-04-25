abcad88c9205bbc33698f9d35ea390f58e35c659
seth-godoy <u443447760@byui.edu>
2022-04-25 14:07:56 -0600

    Added turn for X

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,29 +6,64 @@ Purpose: Create a TUI(text-based user interface) to play Tic-Tac-Toe
 
 
 def main():
-    sqrs = square_values()
-    board = give_board(sqrs)
-    play_game(sqrs, board)
+    squares = create_squares()
+    play_game(squares)
 
 
-def play_game(sqrs, board):
+def play_game(squares):
     end_game = False
-    print(board)
+
+    while end_game == False:
+        valid_choice = False
+        display_board(squares)
+        
+        while valid_choice == False:
+            choice = input('It\'s X\'s turn to choose a square(1-9).\n> ')
+            if choice == 'X' or choice == '0':
+                print('\nYou didn\'t enter a valid number.\n'
+                'Please enter a valid number(1-9) that isn\'t already taken.\n')
+                valid_choice = False 
+            elif choice in squares:
+                valid_choice = True
+            else:
+                print('\nYou didn\'t enter a valid number.\n'
+                'Please enter a valid number(1-9) that isn\'t already taken.\n')
+                valid_choice = False
+
+        squares[squares.index(choice)] = 'X'
+        end_game = check_win(squares, turn='X')
 
 
-def give_board(sqrs):
-    board = '\n' \
-    f'{sqrs[0]} | {sqrs[1]} | {sqrs[2]}\n' \
-    '---------\n' \
-    f'{sqrs[3]} | {sqrs[4]} | {sqrs[5]}\n' \
-    '---------\n' \
-    f'{sqrs[6]} | {sqrs[7]} | {sqrs[8]}\n'
-    return board
+def check_win(squares, turn):
+    if turn == 'X':
+        if \
+        (squares[0] == 'X' and squares[1] == 'X' and squares[2] == 'X') or \
+        (squares[3] == 'X' and squares[4] == 'X' and squares[5] == 'X') or \
+        (squares[6] == 'X' and squares[7] == 'X' and squares[8] == 'X') or \
+        (squares[0] == 'X' and squares[3] == 'X' and squares[6] == 'X') or \
+        (squares[1] == 'X' and squares[4] == 'X' and squares[7] == 'X') or \
+        (squares[2] == 'X' and squares[5] == 'X' and squares[8] == 'X') or \
+        (squares[0] == 'X' and squares[4] == 'X' and squares[8] == 'X') or \
+        (squares[6] == 'X' and squares[4] == 'X' and squares[2] == 'X'):
+            display_board(squares)
+            print('X wins!\nThanks for playing!\n')
+            return True
+        else:
+            return False
 
 
-def square_values():
-    sqrs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    return sqrs
+def display_board(squares):
+    print('\n'
+    f'{squares[0]} | {squares[1]} | {squares[2]}\n'
+    '---------\n'
+    f'{squares[3]} | {squares[4]} | {squares[5]}\n'
+    '---------\n'
+    f'{squares[6]} | {squares[7]} | {squares[8]}\n')
+
+
+def create_squares():
+    squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    return squares
 
 
 if __name__ == '__main__':
