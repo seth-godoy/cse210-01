36e71a12e52d62279d6b5624f769e8de4056fbf5
seth-godoy <u443447760@byui.edu>
2022-04-26 15:24:51 -0600

    Eliminated play_game()

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,69 +8,60 @@ Purpose: Create a Tic-Tac-Toe game to play in a terminal
 
 
 def main():
+    """The main function of the program that runs the game.
+
+    Stores a list of strings of numbers to display in the board and starts
+    a loop to play the game. The loop alternates between turns for X and O
+    to place their symbols on the board. The loop ends when end_game equals True.
+    """
     print('\n---- Tic-Tac-Toe ----')
     print('\nLet the game begin!')
+
     squares = create_squares()
-    play_game(squares)
-
-
-def play_game(squares):
-    """Starts the Tic-Tac-Toe game.
-
-    Uses a while loop to start and continue on the game by: displaying the
-    board with display_board(squares),  alternating between turns for
-    player X and player O, and ending the game when a player wins or a draw happens.
-
-    Args:
-        squares (list): a list that starts with 9 numbers as strings.
-
-    Return: nothing
-    """
     end_game = False
-
     while end_game == False:
         display_board(squares)
-
-        valid_choice = False
-        while valid_choice == False:
-            choice = input('It\'s X\'s turn to choose a square(1-9).\n> ')
-            if choice == 'X' or choice == '0':
-                display_board(squares)
-                print('You didn\'t enter a valid number.\n'
-                'Please enter a valid number(1-9) that isn\'t already taken.\n')
-                valid_choice = False 
-            elif choice in squares:
-                valid_choice = True
-            else:
-                display_board(squares)
-                print('You didn\'t enter a valid number.\n'
-                'Please enter a valid number(1-9) that isn\'t already taken.\n')
-                valid_choice = False
-
+        choice = player_turn(squares, turn='X')
         squares[squares.index(choice)] = 'X'
         end_game = check_win(squares, turn='X')
 
         if end_game == False:
             display_board(squares)
-
-            valid_choice = False
-            while valid_choice == False:
-                choice = input('It\'s O\'s turn to choose a square(1-9).\n> ')
-                if choice == 'X' or choice == '0':
-                    display_board(squares)
-                    print('You didn\'t enter a valid number.\n'
-                    'Please enter a valid number(1-9) that isn\'t already taken.\n')
-                    valid_choice = False 
-                elif choice in squares:
-                    valid_choice = True
-                else:
-                    display_board(squares)
-                    print('You didn\'t enter a valid number.\n'
-                    'Please enter a valid number(1-9) that isn\'t already taken.\n')
-                    valid_choice = False
-            
+            choice = player_turn(squares, turn='O')    
             squares[squares.index(choice)] = 'O'
             end_game = check_win(squares, turn='O')
+
+
+def player_turn(squares, turn):
+    """Fills the board with the appropriate mark of the player's turn.
+
+    Asks player X or O to choose a square where to place their marks.
+    The choice is first validated before returning the value.
+
+    Args:
+        squares (list): a list of strings of numbers, Xs or Os.
+        turn (str): a string to tell who's turn it is.
+
+    Return:
+        choice (str): the square the player has chosen.
+    """
+    valid_choice = False
+    while valid_choice == False:
+        choice = input(f'It\'s {turn}\'s turn to choose a square(1-9).\n> ')
+
+        if choice == 'X' or choice == '0':
+            display_board(squares)
+            print('You didn\'t enter a valid number.\n'
+            'Please enter a valid number(1-9) that isn\'t already taken.\n')
+            valid_choice = False 
+        elif choice in squares:
+            valid_choice = True
+        else:
+            display_board(squares)
+            print('You didn\'t enter a valid number.\n'
+            'Please enter a valid number(1-9) that isn\'t already taken.\n')
+            valid_choice = False
+    return choice
 
 
 def check_win(squares, turn):
@@ -80,79 +71,46 @@ def check_win(squares, turn):
     checks if the board has been filled but nobody has won.
 
     Args:
-        squares (list): a list of strings of either numbers, Xs or Os.
-        turn (str): a string to tell check_win() who's turn it is.
+        squares (list): a list of strings of numbers, Xs or Os.
+        turn (str): a string to check who wins.
 
     Return:
         Boolean: False to continue game; True to end game. 
     """
-    if turn == 'X':
-        if \
-        (squares[0] == 'X' and squares[1] == 'X' and squares[2] == 'X') or \
-        (squares[3] == 'X' and squares[4] == 'X' and squares[5] == 'X') or \
-        (squares[6] == 'X' and squares[7] == 'X' and squares[8] == 'X') or \
-        (squares[0] == 'X' and squares[3] == 'X' and squares[6] == 'X') or \
-        (squares[1] == 'X' and squares[4] == 'X' and squares[7] == 'X') or \
-        (squares[2] == 'X' and squares[5] == 'X' and squares[8] == 'X') or \
-        (squares[0] == 'X' and squares[4] == 'X' and squares[8] == 'X') or \
-        (squares[6] == 'X' and squares[4] == 'X' and squares[2] == 'X'):
-            display_board(squares)
-            print('X wins!\nGood game. Thanks for playing!\n')
-            return True
-
-        else:
-            filled_squares = 0
-            for i in squares:
-                if i == 'X' or i == 'O':
-                    filled_squares += 1
-            
-            if filled_squares == 9:
-                display_board(squares)
-                print('Draw!\nGood game. Thanks for playing!\n')
-                return True
-            else:
-                return False
-
-
-    elif turn == 'O':
-        if \
-        (squares[0] == 'O' and squares[1] == 'O' and squares[2] == 'O') or \
-        (squares[3] == 'O' and squares[4] == 'O' and squares[5] == 'O') or \
-        (squares[6] == 'O' and squares[7] == 'O' and squares[8] == 'O') or \
-        (squares[0] == 'O' and squares[3] == 'O' and squares[6] == 'O') or \
-        (squares[1] == 'O' and squares[4] == 'O' and squares[7] == 'O') or \
-        (squares[2] == 'O' and squares[5] == 'O' and squares[8] == 'O') or \
-        (squares[0] == 'O' and squares[4] == 'O' and squares[8] == 'O') or \
-        (squares[6] == 'O' and squares[4] == 'O' and squares[2] == 'O'):
-            display_board(squares)
-            print('O wins!\nGood game. Thanks for playing!\n')
-            return True
-
-        else:
-            filled_squares = 0
-            for i in squares:
-                if i == 'X' or i == 'O':
-                    filled_squares += 1
-            
-            if filled_squares == 9:
-                display_board(squares)
-                print('Draw!\nGood game. Thanks for playing!\n')
-                return True
-            else:
-                return False
-    
-    else:
-        print('\nInvalid value for turn\n')
+    if \
+    (squares[0] == turn and squares[1] == turn and squares[2] == turn) or \
+    (squares[3] == turn and squares[4] == turn and squares[5] == turn) or \
+    (squares[6] == turn and squares[7] == turn and squares[8] == turn) or \
+    (squares[0] == turn and squares[3] == turn and squares[6] == turn) or \
+    (squares[1] == turn and squares[4] == turn and squares[7] == turn) or \
+    (squares[2] == turn and squares[5] == turn and squares[8] == turn) or \
+    (squares[0] == turn and squares[4] == turn and squares[8] == turn) or \
+    (squares[6] == turn and squares[4] == turn and squares[2] == turn):
+        display_board(squares)
+        print(f'{turn} wins!\nGood game. Thanks for playing!\n')
         return True
+
+    else:
+        filled_squares = 0
+        for i in squares:
+            if i == 'X' or i == 'O':
+                filled_squares += 1
+        
+        if filled_squares == 9:
+            display_board(squares)
+            print('Draw!\nGood game. Thanks for playing!\n')
+            return True
+        else:
+            return False
 
 
 def display_board(squares):
     """Displays the board.
 
     Args:
-        squares (list): a list of strings of either numbers, Xs or Os.
+        squares (list): a list of strings of numbers, Xs or Os.
 
-    Returns: nothing
+    Returns: nothing.
     """
     print('\n'
     f'     {squares[0]} | {squares[1]} | {squares[2]}\n'
@@ -163,7 +121,13 @@ def display_board(squares):
 
 
 def create_squares():
-    """Creates a list to display in the board"""
+    """Creates a list of numbers as strings to display in the board.
+
+    Args: none.
+
+    Returns:
+        squares (list): a list of numbers as strings.
+    """
     squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     return squares
 
